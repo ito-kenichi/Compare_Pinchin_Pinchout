e29e5ec5026fe57947d039f62ff416ce92939272
伊藤健一 <itoukenichi@MacBook-Pro-Developer.local>
2020-09-07 19:40:29 +0900

    react native commit

--- react_native_project/App.js
+++ react_native_project/App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
+import ImageZoom from 'react-native-image-pan-zoom';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <ImageZoom cropWidth={Dimensions.get('window').width}
+               cropHeight={Dimensions.get('window').height}
+               imageWidth={200}
+               imageHeight={200}>
+        <Image style={{width:200, height:200}}
+               source={{uri:'https://www.fujifilm.com/products/digital_cameras/x/fujifilm_x_t1/sample_images/img/index/ff_x_t1_001.JPG'}}/>
+    </ImageZoom>
+);
 }
 
 const styles = StyleSheet.create({
